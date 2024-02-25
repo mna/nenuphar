93687bb88b3ef128eb2cf88e78332f09eb3a068f
Martin Angers <martin.n.angers@gmail.com>
2024-02-25 17:42:24 -0500

    Add options to limit execution

--- lang/types/thread.go
+++ lang/types/thread.go
@@ -16,9 +16,23 @@ type Thread struct {
 	Stdin  io.Reader
 
 	// MaxSteps is the maximum number of "steps", a deliberately unspecified
-	// measure of machine execution time, before the thread is cancelled.
+	// measure of machine execution time, before the thread is cancelled. A value
+	// <= 0 means no limit.
 	MaxSteps int
 
+	// DisableRecursion prevents recursive execution of functions when set to
+	// true. It incurs a small performance cost for the runtime verification on
+	// each function call but can be a useful safety check when executing
+	// untrusted code. If a recursive call is detected when set to true, the
+	// thread is cancelled.
+	DisableRecursion bool
+
+	// MaxCallStackDepth limits the number of nested function calls. If the limit
+	// is reached, the thread is cancelled. A value <= 0 means no limit.
+	MaxCallStackDepth int
+
+	// Load is an optional function value to call to load modules (called by the
+	// LOAD opcode).
 	Load func(*Thread, string) (Value, error)
 
 	ctx       context.Context

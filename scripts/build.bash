79ac693f2cf0ea8c0ef4991d569afe631f31598e
Martin Angers <martin.n.angers@gmail.com>
2024-03-25 19:46:13 -0400

    Add build script

---
+++ scripts/build.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# must run from the root of the repo
+if [[ ! -s ./cmd/nenuphar/main.go ]]; then
+  echo "make sure you are running from the root of the repository"
+  exit 1
+fi
+
+# if there are any uncommitted changes, stop
+if [[ -n "$(git status --porcelain)" ]]; then
+  echo "git repository has uncommitted changes"
+  exit 1
+fi
+
+# get the version information and current date
+VERSION="$(git describe --tags || git rev-parse --short HEAD)"
+DATE="$(date --iso-8601)"
+
+mkdir -p ./bin
+
+go build -o ./bin/nenuphar -ldflags "-X main.version=${VERSION} -X main.buildDate=${DATE}" ./cmd/nenuphar/

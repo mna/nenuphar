7fcc5b8cbbf8fd54f2dfb7d103ea4ff510398d00
Martin Angers <martin.n.angers@gmail.com>
2024-03-29 17:34:14 -0400

    Move to go/token FileSet-based Pos and Position

--- lang/token/pos.go
+++ lang/token/pos.go
@@ -2,75 +2,125 @@ package token
 
 import (
 	"fmt"
+	"go/token"
+	"strconv"
 )
 
+type (
+	File     = token.File
+	FileSet  = token.FileSet
+	Position = token.Position
+	Pos      = token.Pos
+)
+
+const NoPos = token.NoPos
+
+var NewFileSet = token.NewFileSet
+
+// PosMode is the mode that controls printing of position information.
+type PosMode int
+
+// List of supported position modes.
 const (
-	lineBits = 18
-	colBits  = 32 - lineBits
-
-	// MaxLines is the maximum 1-based line number value that can be encoded in
-	// Pos.
-	MaxLines = (1 << lineBits) - 1
-	// MaxCols is the maximum 1-based column number value that can be encoded in
-	// Pos.
-	MaxCols = (1 << colBits) - 1
-
-	lineMask = MaxLines
-	colMask  = MaxCols
+	PosNone    PosMode = iota
+	PosOffsets         // [startoffset:endoffset]
+	PosLong            // [filename:startline:col:endline:col]
+	PosRaw             // [%d:%d] of the raw uninterpreted gotoken.Pos values
 )
 
-// Pos is an efficient encoding of a 1-based line and column position in a
-// 32-bit unsigned integer. A value of 0 for either line or column should be
-// interpreted as "unknown".
-type Pos uint32
-
-// MakePos creates a Pos value encoding the provided line and col. It is the
-// caller's responsibility to ensure the values are > 0 and <= the maximum
-// allowed.
-func MakePos(line, col int) Pos {
-	return Pos(col<<lineBits | line)
+var posLabels = [...]string{
+	PosNone:    "none",
+	PosOffsets: "offsets",
+	PosLong:    "long",
+	PosRaw:     "raw",
 }
 
-// LineCol returns the line and column values encoded in Pos.
-func (p Pos) LineCol() (int, int) {
-	l := p & lineMask
-	c := (p >> lineBits) & colMask
-	return int(l), int(c)
-}
-
-// Unknown returns true if either line or column value is unknown.
-func (p Pos) Unknown() bool {
-	l, c := p.LineCol()
-	return l == 0 || c == 0
-}
-
-func (p Pos) ToPosition(file string, offset int) Position {
-	return Position{Filename: file, Pos: p, Offset: offset}
-}
-
-// Position fully describes a location in a file, including its filename.
-type Position struct {
-	Filename string
-	Pos      Pos
-	Offset   int // offset in bytes
-}
-
-// MakePosition returns a position with the specified components.
-func MakePosition(file string, offset, line, col int) Position {
-	return Position{Filename: file, Pos: MakePos(line, col), Offset: offset}
-}
-
-func (p Position) String() string {
-	file := p.Filename
-	if file == "" {
-		file = "<unknown file>"
+// String returns the string representation of the PosMode.
+func (m PosMode) String() string {
+	if m >= 0 && int(m) < len(posLabels) {
+		return posLabels[m]
 	}
-	l, c := p.Pos.LineCol()
-	if l > 0 {
-		if c > 0 {
-			return fmt.Sprintf("%s:%d:%d", file, l, c)
+	return strconv.Itoa(int(m))
+}
+
+// PosSpan is an interface the defines the method for a value that can report a
+// start-end position span, where the end position is one past the final
+// position (e.g. [1-5) means a value starting at 1 up to and including 4). The
+// length of the span is end - start.
+//
+// All ast.Node types implement this interface.
+type PosSpan interface {
+	Span() (start, end Pos)
+}
+
+// PosCovers returns true if test is fully inside the position span of ref.
+// Both ref and test must be values that belong to the same file.
+func PosInside(ref, test PosSpan) bool {
+	ref0, ref1 := ref.Span()
+	tst0, tst1 := test.Span()
+	return tst0 >= ref0 && tst1 <= ref1
+}
+
+// PosAdjacent returns true if test is "adjacent to" ref following the
+// comment-to-Node association rules documented in the parser package. Both ref
+// and test must be values that belong to the file provided as argument,
+// otherwise it panics.
+//
+// test is considered adjacent to ref if:
+//  1. test.start or test.end is within the range of ref
+//  2. test.end < ref.start but is on the same or the preceding line
+//  3. test.start > ref.end but is on the same line
+func PosAdjacent(ref, test PosSpan, file *File) bool {
+	ref0, ref1 := ref.Span()
+	tst0, tst1 := test.Span()
+
+	if ref0 <= tst1 && ref1 >= tst0 {
+		/* 1. */ return true
+	}
+
+	if tst1 < ref0 {
+		tst1l, ref0l := file.Line(tst1), file.Line(ref0)
+		if tst1l == ref0l || tst1l == ref0l-1 {
+			/* 2. */ return true
 		}
-		return fmt.Sprintf("%s:%d", file, l)
 	}
-	return file
+	if tst0 > ref1 {
+		tst0l, ref1l := file.Line(tst0), file.Line(ref1)
+		if tst0l == ref1l {
+			/* 3. */ return true
+		}
+	}
+	return false
+}
+
+// FormatPos formats the position according to the mode. If filename is false and the
+// mode is PosLong, the filename is not included (useful to print a range of from:to
+// positions, where the filename is already part of the from label).
+func FormatPos(mode PosMode, file *File, pos Pos, filename bool) string {
+	var lbl string
+	switch mode {
+	case PosOffsets:
+		lbl = "-"
+		if pos.IsValid() {
+			lbl = strconv.Itoa(file.Offset(pos))
+		}
+
+	case PosLong:
+		if pos.IsValid() {
+			lpos := file.Position(pos)
+			if filename {
+				lbl = fmt.Sprintf("%s:%d:%d", lpos.Filename, lpos.Line, lpos.Column)
+			} else {
+				lbl = fmt.Sprintf(":%d:%d", lpos.Line, lpos.Column)
+			}
+		} else if filename {
+			lbl = fmt.Sprintf("%s:-:-", file.Name())
+		} else {
+			lbl = ":-:-"
+		}
+
+	case PosRaw:
+		lbl = strconv.Itoa(int(pos))
+	}
+	return lbl
 }
--- lang/token/pos_test.go
+++ lang/token/pos_test.go
@@ -1,33 +1,160 @@
 package token
 
 import (
+	"fmt"
 	"testing"
-
-	"github.com/stretchr/testify/require"
 )
 
-func TestPos(t *testing.T) {
-	for l := 1; l <= MaxLines; l++ {
-		for c := 1; c <= MaxCols; c++ {
-			p := MakePos(l, c)
-			if p.Unknown() {
-				t.Fatalf("p.Unknown() true for %d, %d", l, c)
+type startEnd struct {
+	s, e Pos
+}
+
+func (se startEnd) Span() (start, end Pos) { return se.s, se.e }
+
+func TestPosInside(t *testing.T) {
+	cases := []struct {
+		ref, test startEnd
+		want      bool
+	}{
+		{startEnd{1, 2}, startEnd{3, 4}, false},
+		{startEnd{1, 3}, startEnd{3, 4}, false},
+		{startEnd{1, 4}, startEnd{3, 4}, true},
+		{startEnd{2, 4}, startEnd{3, 4}, true},
+		{startEnd{3, 4}, startEnd{3, 4}, true},
+		{startEnd{4, 5}, startEnd{3, 4}, false},
+	}
+	for _, c := range cases {
+		t.Run(fmt.Sprintf("%v-%v", c.ref, c.test), func(t *testing.T) {
+			got := PosInside(c.ref, c.test)
+			if c.want != got {
+				t.Errorf("want %t, got %t", c.want, got)
 			}
-			gotl, gotc := p.LineCol()
-			if gotl != l || gotc != c {
-				t.Fatalf("p.LineCol() returned %d, %d for %d, %d", gotl, gotc, l, c)
-			}
-		}
+		})
 	}
 }
 
-func TestPosUnknown(t *testing.T) {
-	p := MakePos(0, 0)
-	require.True(t, p.Unknown())
-	p = MakePos(1, 0)
-	require.True(t, p.Unknown())
-	p = MakePos(0, 1)
-	require.True(t, p.Unknown())
-	p = MakePos(1, 1)
-	require.False(t, p.Unknown())
+func TestPosAdjacent(t *testing.T) {
+	fset := NewFileSet()
+	f := fset.AddFile("test", -1, 10)
+	f.AddLine(3) // note that those newlines are in raw byte offsets;
+	f.AddLine(5) // translated to Pos values, you must + 1!
+	f.AddLine(8)
+
+	// In Pos values:
+	// | 1  2  3  4  5  6  7  8  9  10  11 |
+	//   _  _  _  \n _  \n _  _  \n _   EOF
+
+	cases := []struct {
+		ref, test startEnd
+		want      bool
+	}{
+		// both empty, ref before test
+		{startEnd{1, 1}, startEnd{1, 1}, true},
+		{startEnd{1, 1}, startEnd{2, 2}, true},
+		{startEnd{1, 1}, startEnd{3, 3}, true},
+		{startEnd{1, 1}, startEnd{4, 4}, false}, // test is on next line
+		{startEnd{4, 4}, startEnd{4, 4}, true},
+		{startEnd{4, 4}, startEnd{5, 5}, true},
+		{startEnd{4, 4}, startEnd{6, 6}, false}, // test is on next line
+		{startEnd{9, 9}, startEnd{9, 9}, true},
+		{startEnd{9, 9}, startEnd{10, 10}, true},
+		{startEnd{9, 9}, startEnd{11, 11}, true},
+
+		// both empty, ref after test
+		{startEnd{2, 2}, startEnd{1, 1}, true},
+		{startEnd{3, 3}, startEnd{1, 1}, true},
+		{startEnd{4, 4}, startEnd{1, 1}, true},
+		{startEnd{5, 5}, startEnd{1, 1}, true},
+		{startEnd{6, 6}, startEnd{1, 1}, false}, // 2 lines after
+		{startEnd{6, 6}, startEnd{2, 2}, false}, // 2 lines after
+		{startEnd{6, 6}, startEnd{3, 3}, false}, // 2 lines after
+		{startEnd{6, 6}, startEnd{4, 4}, true},
+		{startEnd{7, 7}, startEnd{4, 4}, true},
+		{startEnd{8, 8}, startEnd{4, 4}, true},
+		{startEnd{9, 9}, startEnd{4, 4}, false},   // 2 lines after
+		{startEnd{10, 10}, startEnd{4, 4}, false}, // 2 lines after
+		{startEnd{11, 11}, startEnd{4, 4}, false}, // 2 lines after
+		{startEnd{9, 9}, startEnd{6, 6}, true},
+		{startEnd{10, 10}, startEnd{6, 6}, true},
+		{startEnd{11, 11}, startEnd{6, 6}, true},
+
+		// non-empty
+		{startEnd{4, 8}, startEnd{1, 2}, true},  // same line
+		{startEnd{5, 8}, startEnd{1, 2}, true},  // next line
+		{startEnd{1, 3}, startEnd{3, 4}, true},  // same line
+		{startEnd{1, 3}, startEnd{5, 7}, false}, // after, not same line
+		{startEnd{2, 8}, startEnd{4, 6}, true},  // within
+		{startEnd{2, 8}, startEnd{1, 3}, true},  // partly within
+		{startEnd{2, 8}, startEnd{6, 10}, true}, // partly within
+		{startEnd{3, 4}, startEnd{1, 2}, true},  // same line
+		{startEnd{5, 6}, startEnd{1, 2}, true},  // next line
+		{startEnd{7, 9}, startEnd{1, 2}, false}, // two lines after
+	}
+	for _, c := range cases {
+		t.Run(fmt.Sprintf("%v-%v", c.ref, c.test), func(t *testing.T) {
+			got := PosAdjacent(c.ref, c.test, f)
+			if c.want != got {
+				t.Errorf("want %t, got %t", c.want, got)
+			}
+		})
+	}
+}
+
+func TestFormatPos(t *testing.T) {
+	fset := NewFileSet()
+	f0 := fset.AddFile("test", -1, 10)
+	f1 := fset.AddFile("test_next", -1, 10)
+
+	cases := []struct {
+		pos  Pos
+		mode PosMode
+		file *File
+		want string
+	}{
+		{NoPos, PosLong, f0, "test:-:-"},
+		{NoPos, PosOffsets, f0, "-"},
+		{NoPos, PosRaw, f0, "0"},
+		{NoPos, PosNone, f0, ""},
+		{1, PosLong, f0, "test:1:1"},
+		{1, PosOffsets, f0, "0"},
+		{1, PosRaw, f0, "1"},
+		{1, PosNone, f0, ""},
+		{2, PosLong, f0, "test:1:2"},
+		{2, PosOffsets, f0, "1"},
+		{2, PosRaw, f0, "2"},
+		{2, PosNone, f0, ""},
+		{10, PosLong, f0, "test:1:10"},
+		{10, PosOffsets, f0, "9"},
+		{10, PosRaw, f0, "10"},
+		{10, PosNone, f0, ""},
+		{11, PosLong, f0, "test:1:11"},
+		{11, PosOffsets, f0, "10"},
+		{11, PosRaw, f0, "11"},
+		{11, PosNone, f0, ""},
+		{12, PosLong, f1, "test_next:1:1"},
+		{12, PosOffsets, f1, "0"},
+		{12, PosRaw, f1, "12"},
+		{12, PosNone, f1, ""},
+		{13, PosLong, f1, "test_next:1:2"},
+		{13, PosOffsets, f1, "1"},
+		{13, PosRaw, f1, "13"},
+		{13, PosNone, f1, ""},
+		{-14, PosLong, f1, ":1:3"},
+	}
+
+	for _, c := range cases {
+		t.Run(fmt.Sprintf("%d:%s", c.pos, c.mode), func(t *testing.T) {
+			// negative pos means to set filename to false
+			pos := c.pos
+			fname := true
+			if pos < 0 {
+				pos = -pos
+				fname = false
+			}
+			got := FormatPos(c.mode, c.file, pos, fname)
+			if got != c.want {
+				t.Errorf("want %q, got %q", c.want, got)
+			}
+		})
+	}
 }

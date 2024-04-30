10f8cd7377d597e1a5a246d8bb235675013a4ce6
Martin Angers <martin.n.angers@gmail.com>
2024-04-30 18:21:09 -0400

    Add coverage to token tests

--- lang/token/token_test.go
+++ lang/token/token_test.go
@@ -2,6 +2,8 @@ package token
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestTokenString(t *testing.T) {
@@ -10,4 +12,90 @@ func TestTokenString(t *testing.T) {
 			t.Errorf("missing string representation of token %d", tok)
 		}
 	}
+}
+
+func TestLookupKw(t *testing.T) {
+	for tok := Token(0); tok <= maxToken; tok++ {
+		expect := tok >= kwStart && tok <= kwEnd
+		val := LookupKw(tok.GoString())
+		if expect {
+			require.Equal(t, tok, val)
+		} else {
+			require.Equal(t, IDENT, val)
+		}
+	}
+}
+
+func TestLookupPunct(t *testing.T) {
+	for tok := Token(0); tok <= maxToken; tok++ {
+		expect := tok >= punctStart && tok <= punctEnd
+		val := LookupPunct(tok.String())
+		if expect {
+			require.Equal(t, tok, val)
+		} else {
+			require.Equal(t, ILLEGAL, val)
+		}
+	}
+}
+
+func TestIsAugBinop(t *testing.T) {
+	for tok := Token(0); tok <= maxToken; tok++ {
+		expect := tok >= augopStart && tok <= augopEnd
+		got := tok.IsAugBinop()
+		require.Equal(t, expect, got)
+	}
+}
+
+func TestIsBinop(t *testing.T) {
+	for tok := Token(0); tok <= maxToken; tok++ {
+		maybe := (tok >= punctStart && tok <= punctEnd && !tok.IsAugBinop()) || tok == AND || tok == OR
+		got := tok.IsBinop()
+		if !maybe {
+			require.False(t, got)
+		}
+	}
+}
+
+func TestIsUnop(t *testing.T) {
+	for tok := Token(0); tok <= maxToken; tok++ {
+		maybe := (tok >= punctStart && tok <= punctEnd && !tok.IsAugBinop()) || tok == NOT || tok == TRY || tok == MUST
+		got := tok.IsUnop()
+		if !maybe {
+			require.False(t, got)
+		}
+	}
+}
+
+func TestIsAtom(t *testing.T) {
+	for tok := Token(0); tok <= maxToken; tok++ {
+		maybe := (tok >= litStart && tok <= litEnd) || tok == NULL || tok == TRUE || tok == FALSE
+		got := tok.IsAtom()
+		if !maybe {
+			require.False(t, got)
+		}
+	}
+}
+
+func TestLiteral(t *testing.T) {
+	val := Value{
+		Raw:    "ident",
+		String: "string",
+		Int:    1,
+		Float:  2,
+	}
+
+	got := IDENT.Literal(val)
+	require.Equal(t, val.Raw, got)
+	got = STRING.Literal(val)
+	require.Equal(t, `"string"`, got)
+	got = COMMENT.Literal(val)
+	require.Equal(t, val.String, got)
+	got = INT.Literal(val)
+	require.Equal(t, "1", got)
+	got = INT.Literal(val)
+	require.Equal(t, "1", got)
+	got = FLOAT.Literal(val)
+	require.Equal(t, "2", got)
+	got = ILLEGAL.Literal(val)
+	require.Equal(t, "", got)
 }
